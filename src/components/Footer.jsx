f268b94a85404b7ca9bba6324b4d3ab301914b0b
kartik saxena <kash795679@gmail.com>
2024-11-20 19:25:32 +0530

    second commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
       <footer className="h-28 flex flex-col gap-5 justify-center items-center">
         <h1 className="text-white">Contact - +918949405289</h1>
-
+        <h1>Full stack developer</h1>
         <p className="text-center text-white">
           &lt;&lt;&lt;p &copy; Kartik Saxena&gt;&gt;&gt;
         </p>
